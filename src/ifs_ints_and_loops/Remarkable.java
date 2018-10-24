ac640577e4d042765cd2d23197946e3cf17cdb60
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-24 13:55:45 -0700

    Unbirthday, Remarkable

---
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -0,0 +1,11 @@
+package ifs_ints_and_loops;
+
+public class Remarkable {
+public static void main(String[] args) {
+	String Mohammad="Cant write";
+	String Angel="is overly slanted";
+	String Mohamma="Has leukepemia";
+	String Thomas="Killed 17 people in the shooting";
+	String name=JOptionPane.showInputDialog("Omae wa mo shindeiru");
+}
+} 
---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,13 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+JOptionPane.showInputDialog("When is your birthday?");
+String birthday=JOptionPane.showInputDialog("When is your birthday?");
+if(birthday.equals(10/4));}
+}
+else {
+JOptionPane.showMessageDialog(null,"very marry unbirthday!"); }
+	
